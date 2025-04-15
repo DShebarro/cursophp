8e364f207ff4741ceafbd0c8587b1881e2e956c7
Davy Shebarro Araujo de Souza <03155205237@CREA-AM.LOCAL>
2025-04-15 15:19:07 -0400

    Curso PHP

---
+++ ex001/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dados do Servidor</title> // Título da aba
+</head>
+
+<body>
+    <h1>Dados de Servidor</h1> // Título de nível 1
+    <?
+        phpinfo(); // Dados de onde o script está armazenado (Servidor)
+    ?>
+</body>
+
+</html>
