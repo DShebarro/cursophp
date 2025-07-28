425feebc8f98e5dfcdeca57dd7719abd67b6f40d
DShebarro <yvda.shebs20@gmail.com>
2025-07-28 15:05:38 -0400

    Desafio 01

---
+++ treinamento/desafios01/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./style.css">
+    <title>Desafio 01 - Ant e Suc</title>
+</head>
+<body>
+    <header>
+        <h1>Informe um número</h1>
+    </header>
+    <section>
+        <form action="result.php" method="get">
+            <label for="numero">Número:</label>
+            <input type="number" name="numero" required>
+            <br>
+            <input type="submit" value="Calcular">
+        </form>        
+    </section>
+</body>
+</html>
